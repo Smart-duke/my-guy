e8c60ead0cbdc1054d051be7abf3c2d39caade74
Precious Longe <preciuzlp943@gmail.com>
2024-02-20 03:07:48 +0100

    Testing responsiveness 2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,17 +1,21 @@
 import { Link, NavLink } from "react-router-dom";
-// import { FaBars } from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./NavBar.css";
 import logoImg from "../assets/image.png";
 import settingsImg from "../assets/Vector.png";
+import { useState } from "react";
 
 const NavBar = () => {
+  const [navDrop, setNavDrop] = useState(false);
+  const handleDrop = () => setNavDrop(!navDrop);
+
   return (
-    <nav className="header">
+    <nav className="headerr">
       <Link to="/">
         <img src={logoImg} alt="logo" className="logo-img" />
       </Link>
       <div className="nav-container">
-        <ul className="nav-list">
+        <ul className={navDrop ? "nav-list navshow" : "nav-list"}>
           <li>
             <Link to={"/"}>Home</Link>
           </li>
@@ -37,6 +41,13 @@ const NavBar = () => {
         <NavLink to="/">
           <button className="join-btn">Join</button>
         </NavLink>
+      </div>
+      <div className="hamburger" onClick={handleDrop}>
+        {navDrop ? (
+          <FaTimes size={20} style={{ color: "#fff" }} />
+        ) : (
+          <FaBars size={20} style={{ color: "#fff" }} />
+        )}
       </div>
     </nav>
   );
--- src/routes/Testimonial.js
+++ src/routes/Testimonial.js
@@ -1,26 +1,32 @@
 import React from "react";
 import "./Testimonial.css";
-import CeoImg from "../assets/ceo.png"
+import CeoImg from "../assets/ceo.png";
 import { FaStar } from "react-icons/fa";
 
-
 const Testimonial = () => {
-  return <div className="testimonial">
+  return (
+    <div className="testimonial">
       <img src={CeoImg} alt="the CEO MyGuy" className="ceo-img" />
       <div className="testimonial-text">
         <h3>What our customers are saying</h3>
-        <p className="testi">Our success is determined not only by what we achieve but also by the satisfaction and trust of our valued customers.</p>
-        <div>
-        <p className="testifier">Afekhare Paul</p>
-        <FaStar size={10} style={{color:"#047711"}} />
-        <FaStar size={10} style={{color:"#047711"}} />
-        <FaStar size={10} style={{color:"#047711"}} />
-        <FaStar size={10} style={{color:"#047711"}} />
-        <FaStar size={10} style={{color:"#047711"}} />
-        <p className="post">CEO</p>
+        <p className="testimony">
+          Our success is determined not only by what we achieve but also by the
+          satisfaction and trust of our valued customers.
+        </p>
+        <div className="down">
+          <p className="testifier">Afekhare Paul</p>
+          <div>
+          <FaStar size={10} style={{ color: "#047711" }} className="star" />
+          <FaStar size={10} style={{ color: "#047711" }} className="star" />
+          <FaStar size={10} style={{ color: "#047711" }} className="star" />
+          <FaStar size={10} style={{ color: "#047711" }} className="star" />
+          <FaStar size={10} style={{ color: "#047711" }} className="star" />
+          </div>
+          <p className="post">CEO</p>
         </div>
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default Testimonial;
